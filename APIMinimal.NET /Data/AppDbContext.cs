cb4768a6b481024687e30b475facd90cb4cdd832
João Guilherme Dias <Alwaysstrong08@hotmail.com>
2024-08-18 13:19:49 -0300

    Create AppDbContext.cs

---
+++ APIMinimal.NET /Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using ApiCrud.Estudantes;
+
+
+
+namespace ApiCrud.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Estudante> Estudantes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(connectionString: "Data Source=Banco.sqlite");
+            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+            optionsBuilder.EnableSensitiveDataLogging();
+
+            base.OnConfiguring(optionsBuilder);
+
+        }
+    }
+ }
+
